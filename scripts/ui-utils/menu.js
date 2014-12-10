7e776642269e157789d94b6b0d5b0cd22e3abe02
thinhit <thinhit@yahoo.com>
2014-12-10 15:52:46 +0700

    update swipe menu

--- scripts/ui-utils/menu.js
+++ scripts/ui-utils/menu.js
@@ -1,19 +1,105 @@
+
 angular.module('Pow')
-    
-    .directive('myMenu', function ($rootScope) {
+   
+    .directive('powMenu', function ($rootScope) {
         return {
             restrict: 'ECMA',
             transclude: false,
             link: function (scope, el, attr) {
                 var navMenu = angular.element(el).find('nav');
-                var open = false;
+                var open = false,
+                clickEventType= document.ontouchstart !== null ? 'mousedown' : 'touchstart',
+                moveEventType = document.ontouchmove  !== null ? 'mousemove' : 'touchmove' ,
+                endEventType  = document.ontouchend   !== null ? 'mouseup'   : 'touchend'  ;
+                navMenu.addClass('menu-animate');
+                $('.main-container').on(clickEventType, function (ev){
+                    
+                    var startAt = 0,
+                        endAt, 
+                        posX = 0;
+
+
+                    if(ev.hasOwnProperty('touches') && ev.touches[0].pageX < 5){
+                        console.log(ev.touches[0].pageX)
+                        ev.preventDefault();
+                        
+                        var moveHandler = function (e){
+                                posX = e.pageX - startAt;
+
+
+                                if(moveEventType === 'touchmove') {
+                                    posX = e.touches[0].pageX - startAt;
+                                }
+                                posX = (Math.min( Math.max(0, posX), 250 - startAt)) -250;
+                                
+                                document.getElementById('mana').innerHTML = posX;
+
+                            if(!navMenu.hasClass('open')){
+                                navMenu.removeClass('menu-animate');
+                                navMenu.css({
+                                    /*left:posX + 'px'*/
+                                    transform: 'translate3d(' + (posX) + 'px, 0, 0)',
+                                    '-webkit-transform': 'translate3d(' + (posX) + 'px, 0, 0)'
+                                });
+                            }
+                        },
+                        stopHandler = function () {
+                            $('.main-container').off(moveEventType);
+                            $('.main-container').off(endEventType);
+
+                            console.log('blabla');
+                            document.getElementById('mana').innerHTML = 'Touch end' + posX + ' --- ' + 250/2 + '  ++++   ' + ((250/2) + posX) ;
+                            navMenu.addClass('menu-animate');
+                            if(((250/2) + posX) > 0){
+
+                                navMenu.addClass('open');
+
+                            }else {
+                                if(navMenu.hasClass('open')){
+                                    navMenu.removeClass('open');
+                                }else {
+                                    navMenu.css({
+                                        transform: 'translate3d(-250px, 0, 0)',
+                                        '-webkit-transform': 'translate3d(-250px, 0, 0)'
+                                    });
+                                }
+                            }
+                            posX = 0
+                            
+                        };
+
+
+
+
+                        if(clickEventType == 'touchstart'){
+                            startAt  = ev.touches[0].pageX;
+                        }   
+
+                        $('.main-container').on(moveEventType, moveHandler);
+
+
+                        $('.main-container').on(endEventType, function (){
+                            stopHandler()
+                        })
+                    }
+                    
+
+                })
 
                 $rootScope.$on('OPEN_NAV_MENU', function () {
                     navMenu.addClass('open');
+                    navMenu.css({
+                        transform: '',
+                        '-webkit-transform': ''
+                    });
                 });
 
                 $rootScope.$on('CLOSE_NAV_MENU', function () {
                     navMenu.removeClass('open');
+                    navMenu.css({
+                        transform: '',
+                        '-webkit-transform': ''
+                    });
                 });
 
 
@@ -25,9 +111,9 @@ angular.module('Pow')
             restrict: 'ECMA',
             transclude: false,
             link: function (scope, el, attr) {
-                console.log('blabla')
                 angular.element(el).on('click', function () {
-                    var menuEl = document.querySelectorAll('my-menu'),
+
+                    var menuEl = document.querySelectorAll('pow-menu'),
                         menuHasOpen = angular.element(menuEl).find('nav').hasClass('open');
 
                     if (!menuHasOpen) {
@@ -48,7 +134,7 @@ angular.module('Pow')
             link: function (scope, el, attr) {
 
                 angular.element(el).on('click', function () {
-                    var menuEl = document.querySelectorAll('my-menu'),
+                    var menuEl = document.querySelectorAll('pow-menu'),
                         menuHasOpen = angular.element(menuEl).find('nav').hasClass('open');
 
                     $rootScope.$broadcast('CLOSE_NAV_MENU');
